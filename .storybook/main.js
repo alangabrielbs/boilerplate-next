54b6e707bfa012b85f00c03d628eef379686cabf
Alan Gabriel <contatoalangabriel@gmail.com>
2020-11-15 00:44:16 -0300

    remove index the storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,4 @@
 module.exports = {
-  stories: [
-    // "../src/**/*.stories.mdx",
-    '../src/components/**/stories.tsx'
-  ],
+  stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials']
 }
